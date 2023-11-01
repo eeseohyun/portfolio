48da938c0ff5dac602e691a85f30bd9d7f3e360a
eeseohyun <jinlijinyoung@gmail.com>
2023-11-01 19:57:06 +0900

    모달 띄우기 및 imageSlider 구현

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ skillsSortItems.forEach((item) => {
 
 //match
 const matchElements = document.querySelectorAll('.match');
+const modalElements = document.querySelectorAll('.modal_wrapper');
 
 matchElements.forEach((match, index) => {
   match.addEventListener('mouseover', (e) => {
@@ -53,4 +54,78 @@ matchElements.forEach((match, index) => {
       }
     });
   });
+  match.addEventListener('click', (e) => {
+    e.preventDefault();
+    modalElements.forEach((modal, index) => {
+      modal.classList.toggle('open');
+    });
+  });
 });
+
+function imageSlider(parent, images) {
+  let currentImage = 0;
+  let slider = {
+    parent: parent,
+    images: parent.querySelector('.images'),
+    thumbnails: parent.querySelector('.thumbnails'),
+    backBtn: parent.querySelector('.back-btn'),
+    nextBtn: parent.querySelector('.next-btn'),
+  };
+
+  slider.images = parent.querySelector('.images');
+  slider.thumbnails = parent.querySelector('.thumbnails');
+
+  slider.images.innerHTML = images
+    .map((image) => {
+      return `<img src="${image}" />`;
+    })
+    .join('');
+
+  let imageNodes = slider.images.querySelectorAll('img');
+  imageNodes[currentImage].classList.add('active');
+
+  slider.thumbnails.innerHTML = slider.images.innerHTML;
+
+  let thumbnailNodes = slider.thumbnails.querySelectorAll('img');
+  thumbnailNodes[currentImage].classList.add('active');
+
+  for (let i = 0; i < thumbnailNodes.length; i++) {
+    thumbnailNodes[i].addEventListener('click', function () {
+      slider.thumbnails.querySelector('img.active').classList.remove('active');
+      imageNodes[currentImage].classList.remove('active');
+      currentImage = i;
+      imageNodes[currentImage].classList.add('active');
+      thumbnailNodes[currentImage].classList.add('active');
+    });
+  }
+
+  slider.backBtn.addEventListener('click', function () {
+    imageNodes[currentImage].classList.remove('active');
+    currentImage--;
+    if (currentImage < 0) {
+      currentImage = images.length - 1;
+    }
+    imageNodes[currentImage].classList.add('active');
+    slider.thumbnails.querySelector('img.active').classList.remove('active');
+    thumbnailNodes[currentImage].classList.add('active');
+  });
+
+  slider.nextBtn.addEventListener('click', function () {
+    imageNodes[currentImage].classList.remove('active');
+    currentImage = (currentImage + 1) % images.length;
+    imageNodes[currentImage].classList.add('active');
+    slider.thumbnails.querySelector('img.active').classList.remove('active');
+    thumbnailNodes[currentImage].classList.add('active');
+  });
+}
+
+let images = [
+  'img/mountaineer1.png',
+  'img/mountaineer2.png',
+  'img/mountaineer3.png',
+  'img/mountaineer4.png',
+  'img/mountaineer5.png',
+  'img/mountaineer6.png',
+];
+
+imageSlider(document.querySelector('.image_slider'), images);

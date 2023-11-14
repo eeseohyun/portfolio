cbc3ba46ff7dfc69c33e08b567e8eb522744c9f0
eeseohyun <jinlijinyoung@gmail.com>
2023-11-14 17:12:27 +0900

    수정

--- main.js
+++ main.js
@@ -1,82 +1,89 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   var sectionLinks = document.querySelector('.section-link');
-//   const sections = document.querySelector('section');
+var sectionLinks = document.querySelectorAll('.section-link');
+const sections = document.querySelectorAll('section');
 
-//   sectionLinks = Array.from(sectionLinks);
-
-//   sectionLinks.forEach((link, index) => {
-//     link.addEventListener('click', (e) => {
-//       e.preventDefault();
-//       sections.forEach((section) => {
-//         section.style.display = 'none';
-//       });
-
-//       sections[index].style.display = 'block';
-//     });
-//   });
-// });
-
-const allSkillsLists = document.querySelectorAll('.skills_lists');
-allSkillsLists.forEach((list) => {
-  list.style.display = 'none';
+sectionLinks.forEach((link, index) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    sections.forEach((section, sectionIndex) => {
+      if (sectionIndex === index) {
+        section.classList.add('active');
+      } else {
+        section.classList.remove('active');
+      }
+    });
+    resetSkills();
+  });
 });
 
-const skillsSortItems = document.querySelectorAll('.skills_sort li');
-skillsSortItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    allSkillsLists.forEach((list) => {
-      list.style.display = 'none';
-    });
-
+// skills
+const skillsSorts = document.querySelectorAll('.skills_sort li');
+const skillList = document.querySelectorAll('.skills_lists');
+skillsSorts.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
     const category = item.getAttribute('data-category');
-
-    const selectedSkillsList = document.querySelector(
-      `.skills_lists.${category}`
-    );
-    if (selectedSkillsList) {
-      selectedSkillsList.style.display = 'block';
+    const selectedList = document.querySelector(`.skills_lists.${category}`);
+    if (selectedList) {
+      selectedList.classList.toggle('active');
     }
+    skillsSorts.forEach((sortItem) => {
+      sortItem.classList.remove('clicked');
+    });
+    item.classList.toggle('clicked');
   });
 });
 
-//match
-const matchElements = document.querySelectorAll('.match');
-const modalElements = document.querySelectorAll('.modal_wrapper');
-
-matchElements.forEach((match, index) => {
-  match.addEventListener('mouseover', (e) => {
-    e.preventDefault();
-    match.classList.toggle('active');
-
-    matchElements.forEach((other, otherIndex) => {
-      if (otherIndex !== index) {
-        other.classList.remove('active');
-      }
-    });
+//클릭 스타일 리셋
+function resetSkills() {
+  skillList.forEach((list) => {
+    list.classList.remove('clicked');
   });
-  match.addEventListener('click', (e) => {
+  skillsSorts.forEach((sortItem) => {
+    sortItem.classList.remove('clicked');
+  });
+}
+
+const matches = document.querySelectorAll('.match');
+const modals = document.querySelectorAll('.modal_wrapper');
+const btnCloses = document.querySelectorAll('.btn-close');
+
+matches.forEach((match, index) => {
+  match.addEventListener('mouseover', () => toggleActive(index));
+  match.addEventListener('click', () => toggleModal(index));
+});
+
+btnCloses.forEach((btnClose, btnIdx) => {
+  btnClose.addEventListener('click', (e) => {
     e.preventDefault();
-    modalElements.forEach((modal, modalIndex) => {
-      if (modalIndex === index) {
-        modal.classList.toggle('open');
-      }
-    });
+    closeModal(btnIdx);
   });
 });
 
+function toggleActive(index) {
+  matches.forEach((other, otherIndex) => {
+    other.classList.toggle('active', otherIndex === index);
+  });
+}
+
+function toggleModal(index) {
+  modals.forEach((modal, modalIndex) => {
+    modal.classList.toggle('open', modalIndex === index);
+  });
+}
+
+function closeModal(index) {
+  modals[index].classList.remove('open');
+}
+
+//이미지 슬라이드
 function imageSlider(parent, images) {
   let currentImage = 0;
   let slider = {
     parent: parent,
     images: parent.querySelector('.images'),
-    thumbnails: parent.querySelector('.thumbnails'),
     backBtn: parent.querySelector('.back-btn'),
     nextBtn: parent.querySelector('.next-btn'),
   };
-
-  slider.images = parent.querySelector('.images');
-  slider.thumbnails = parent.querySelector('.thumbnails');
-
   slider.images.innerHTML = images
     .map((image) => {
       return `<img src="${image}" />`;
@@ -85,21 +92,6 @@ function imageSlider(parent, images) {
 
   let imageNodes = slider.images.querySelectorAll('img');
   imageNodes[currentImage].classList.add('active');
-
-  slider.thumbnails.innerHTML = slider.images.innerHTML;
-
-  let thumbnailNodes = slider.thumbnails.querySelectorAll('img');
-  thumbnailNodes[currentImage].classList.add('active');
-
-  for (let i = 0; i < thumbnailNodes.length; i++) {
-    thumbnailNodes[i].addEventListener('click', function () {
-      slider.thumbnails.querySelector('img.active').classList.remove('active');
-      imageNodes[currentImage].classList.remove('active');
-      currentImage = i;
-      imageNodes[currentImage].classList.add('active');
-      thumbnailNodes[currentImage].classList.add('active');
-    });
-  }
 
   slider.backBtn.addEventListener('click', function () {
     imageNodes[currentImage].classList.remove('active');
@@ -121,7 +113,7 @@ function imageSlider(parent, images) {
   });
 }
 
-let images = [
+let mountaineerImages = [
   'img/mountaineer1.png',
   'img/mountaineer2.png',
   'img/mountaineer3.png',
@@ -130,4 +122,32 @@ let images = [
   'img/mountaineer6.png',
 ];
 
-imageSlider(document.querySelector('.image_slider'), images);
+let twtImages = [
+  'img/twt1.png',
+  'img/twt2.png',
+  'img/twt3.png',
+  'img/twt4.png',
+  'img/twt5.png',
+  'img/twt6.png',
+  'img/twt7.png',
+];
+
+let daengImages = [
+  'img/댕근이다옹1.png',
+  'img/댕근이다옹2.png',
+  'img/댕근이다옹3.png',
+  'img/댕근이다옹4.png',
+  'img/댕근이다옹5.png',
+  'img/댕근이다옹6.png',
+  'img/댕근이다옹7.png',
+];
+
+imageSlider(document.querySelector('.TWT .image_slider'), twtImages);
+imageSlider(
+  document.querySelector('.Mountaineer .image_slider'),
+  mountaineerImages
+);
+imageSlider(
+  document.querySelector('.DangGeuneDaong .image_slider'),
+  daengImages
+);
